4435bed783af293df56cac365b500c2d57d8c90c
nzommmo <110990728+nzommmo@users.noreply.github.com>
2025-02-04 14:55:30 +0300

    Tailwind Variables

---
+++ FRONTEND/src/Components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='text-custom'>Home
+    <h1>Welcome <span>Home</span></h1>
+
+
+    
+    </div>
+  )
+}
+
+export default Home
