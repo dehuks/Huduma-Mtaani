f1af693af05a9241fc24c7decf23ae1d21ed7f3c
nzommmo <110990728+nzommmo@users.noreply.github.com>
2025-02-04 11:09:48 +0300

    Frontend Setup

--- BACKEND/obj/Debug/net9.0/BACKEND.AssemblyInfo.cs
+++ BACKEND/obj/Debug/net9.0/BACKEND.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BACKEND")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2568d6612f1dff9bdc854053c2951c7ef2da5d54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40bf00aadae099b872d50acf50898922bbe97687")]
 [assembly: System.Reflection.AssemblyProductAttribute("BACKEND")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BACKEND")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
