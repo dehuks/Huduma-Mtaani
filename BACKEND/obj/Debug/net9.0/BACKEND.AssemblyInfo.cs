1a436c8144420098cc0b64ab47b516f3b2ba7ffe
dehuks <stevehuko@gmail.com>
2025-02-19 22:35:54 +0300

    Added swagger for API testing

--- BACKEND/obj/Debug/net9.0/BACKEND.AssemblyInfo.cs
+++ BACKEND/obj/Debug/net9.0/BACKEND.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BACKEND")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa5529868b2f1ada60c35583d7256984dfb76d26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee49c60ab9e2c5c1b86c042d133a06a06d20ddfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("BACKEND")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BACKEND")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
