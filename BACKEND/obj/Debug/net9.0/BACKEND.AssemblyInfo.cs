b2c456045953236f1156891e166af791b3e0f99e
dehuks <stevehuko@gmail.com>
2025-02-20 01:11:00 +0300

    Set up Auth controller and configured jwt and swagger

--- BACKEND/obj/Debug/net9.0/BACKEND.AssemblyInfo.cs
+++ BACKEND/obj/Debug/net9.0/BACKEND.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BACKEND")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a436c8144420098cc0b64ab47b516f3b2ba7ffe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82f83bb0f06a300c64fe36a3bf6fd2bcb1925459")]
 [assembly: System.Reflection.AssemblyProductAttribute("BACKEND")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BACKEND")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
